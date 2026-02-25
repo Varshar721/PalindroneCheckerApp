d37d9d4d6210bf00068de4b63a18cb501e202d76
Varshar721 <v4r.sh4.21@gmail.com>
2026-02-25 16:08:39 +0530

    [Varsha] Added >> [Varshar721]

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,44 +1,64 @@
 import java.util.Stack;
 import java.util.ArrayDeque;
 import java.util.Deque;
-
 public class PalindroneCheckerApp {
     public static void main(String[] args) {
         String input = "Level";
-        PalindromeStrategy strategy;
-        strategy = new StackStrategy();
-        boolean result = strategy.check(input);
         System.out.println("Input : " + input);
-        System.out.println("Is Palindrome : " + result);
-    }
-}
+        long start1 = System.nanoTime();
+        boolean result1 = twoPointerCheck(input);
+        long end1 = System.nanoTime();
+        long start2 = System.nanoTime();
+        boolean result2 = stackCheck(input);
+        long end2 = System.nanoTime();
+        long start3 = System.nanoTime();
+        boolean result3 = dequeCheck(input);
+        long end3 = System.nanoTime();
+        System.out.println("\nResults:");
+        System.out.println("Two Pointer  -> " + result1 +
+                " | Time: " + (end1 - start1) + " ns");
 
-interface PalindromeStrategy {
-    boolean check(String input);
-}
-class StackStrategy implements PalindromeStrategy {
-    @Override
-    public boolean check(String input) {
-        Stack<Character> stack = new Stack<>();
-        for (char c : input.toCharArray()) {
-            stack.push(Character.toLowerCase(c));
+        System.out.println("Stack        -> " + result2 +
+                " | Time: " + (end2 - start2) + " ns");
+
+        System.out.println("Deque        -> " + result3 +
+                " | Time: " + (end3 - start3) + " ns");
+    }
+    public static boolean twoPointerCheck(String input) {
+        int start = 0;
+        int end = input.length() - 1;
+
+        while (start < end) {
+            if (Character.toLowerCase(input.charAt(start)) !=
+                    Character.toLowerCase(input.charAt(end))) {
+                return false;
+            }
+            start++;
+            end--;
         }
-        for (char c : input.toCharArray()) {
-            if (Character.toLowerCase(c) != stack.pop()) {
+        return true;
+    }
+    public static boolean stackCheck(String input) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : input.toLowerCase().toCharArray()) {
+            stack.push(c);
+        }
+
+        for (char c : input.toLowerCase().toCharArray()) {
+            if (c != stack.pop()) {
                 return false;
             }
         }
         return true;
     }
-}
-
-class DequeStrategy implements PalindromeStrategy {
-    @Override
-    public boolean check(String input) {
+    public static boolean dequeCheck(String input) {
         Deque<Character> deque = new ArrayDeque<>();
+
         for (char c : input.toLowerCase().toCharArray()) {
             deque.addLast(c);
         }
+
         while (deque.size() > 1) {
             if (!deque.removeFirst().equals(deque.removeLast())) {
                 return false;
