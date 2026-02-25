b9ab3da618f783b3fd89854b5a9bd9ac5b2535dd
Varshar721 <v4r.sh4.21@gmail.com>
2026-02-25 16:03:51 +0530

    [Varsha] Added >> [Varshar721]

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,26 +1,48 @@
+import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class PalindroneCheckerApp {
-
     public static void main(String[] args) {
-
-        String input = "madam";
-        PalindromeService service = new PalindromeService();
-
-        boolean result = service.checkPalindrome(input);
-
+        String input = "Level";
+        PalindromeStrategy strategy;
+        strategy = new StackStrategy();
+        boolean result = strategy.check(input);
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome : " + result);
     }
 }
-class PalindromeService {
-    public boolean checkPalindrome(String input) {
-        int start = 0;
-        int end = input.length() - 1;
-        while (start < end) {
-            if (input.charAt(start) != input.charAt(end)) {
+
+interface PalindromeStrategy {
+    boolean check(String input);
+}
+class StackStrategy implements PalindromeStrategy {
+    @Override
+    public boolean check(String input) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : input.toCharArray()) {
+            stack.push(Character.toLowerCase(c));
+        }
+        for (char c : input.toCharArray()) {
+            if (Character.toLowerCase(c) != stack.pop()) {
                 return false;
             }
-            start++;
-            end--;
+        }
+        return true;
+    }
+}
+
+class DequeStrategy implements PalindromeStrategy {
+    @Override
+    public boolean check(String input) {
+        Deque<Character> deque = new ArrayDeque<>();
+        for (char c : input.toLowerCase().toCharArray()) {
+            deque.addLast(c);
+        }
+        while (deque.size() > 1) {
+            if (!deque.removeFirst().equals(deque.removeLast())) {
+                return false;
+            }
         }
         return true;
     }
